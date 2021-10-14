2dc7ba5444d57861f408526529054cb4659709bf
zhangjie0728 <92006473+zhangjie0728@users.noreply.github.com>
2021-10-14 14:44:06 +0800

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,30 @@
+'''
+任务：猜3次就睡眠 time.sleep(10)
+'''
+import random
+import  time
+import  os
+shuzi=random, randint(1,10)
+while 1:
+    print("这是一个猜数字游戏，答案是1到10的整数")
+    count=0
+    while count<3
+        num=input("time sleep")
+        if num==daan:
+            print("恭喜你答对了")
+            break
+        if  num>1
+            print("猜错了，继续加油")
+            num=int(num)
+        if  num<10
+              print("猜错了继续加油")
+        else:
+            time.sleep(10)
+
+
+
+
+
+
+
+
